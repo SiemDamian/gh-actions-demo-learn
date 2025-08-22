33efbc44e75d796162c82c4b49bdc555b142338b
ds1 <damian.siemieniec@softnet.com.pl>
2025-08-22 11:08:36 +0200

    tes2

--- py-upper/test_uppercase.py
+++ py-upper/test_uppercase.py
@@ -3,7 +3,7 @@ from uppercase import to_uppercase
 
 def test_to_uppercase():
     assert to_uppercase("hello") == "HELLO"
-    assert to_uppercase("PytHon") == "PYTHON"
+    assert to_uppercase("Python") == "PYTHON"
     assert to_uppercase("123abc") == "123ABC"
     assert to_uppercase("") == ""   # Test pustego napisu
 
